a79093f89453c842712f6de3736ee65c427b7d7d
vvdiduh <vvdiduh@gmail.com>
2022-12-06 02:04:50 +0200

    finish 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+
+let timerId = 0;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', editBgColor);
+refs.stopBtn.addEventListener('click', stopIntrval)
+
+function stopIntrval() {
+    clearInterval(timerId)
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
+
+function backgroudColor() {
+    refs.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function editBgColor() {
+    timerId = setInterval(backgroudColor, 500);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
 
